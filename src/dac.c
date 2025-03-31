0a25a673bf27cedac24eb5651162a27ccfd4a22b
Advay Welling <advaywelling@gmail.com>
2025-03-31 16:51:25 -0400

    blu

--- src/dac.c
+++ src/dac.c
@@ -1,7 +1,9 @@
 #include "stm32f0xx.h"
 #define RATE 8000
+
 //setting up DAC for sound output
 const int freqs[] = {650, 480, 590, 780}; // 4 notes
+
 void setup_dac(void) {
     RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
     GPIOA->MODER |= 0x0300; // PA4 as analog mode
@@ -10,6 +12,7 @@ void setup_dac(void) {
     DAC->CR |= DAC_CR_TEN1 | DAC_CR_DMAEN1;
     DAC->CR |= DAC_CR_EN1;
 }
+
 void init_tim6(void) {
     RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
     TIM6->PSC = 48 - 1;
@@ -18,7 +21,9 @@ void init_tim6(void) {
     TIM6->CR2 |= TIM_CR2_MMS_1;
     TIM6->CR1 |= TIM_CR1_CEN;
 }
+
 uint16_t audio[RATE];
+
 void setup_dma(void) {
     RCC->AHBENR |= RCC_AHBENR_DMA1EN;
     DMA1_Channel3->CCR &= ~DMA_CCR_EN;
@@ -45,4 +50,14 @@ void tetris_sound(void) {
             audio[idx++] = (j % (RATE / freq) < (RATE / (2 * freq))) ? 3000 : 1000;
         }
     }
+}
+
+int main(void) {
+    setup_dac();
+    setup_dma();
+    init_tim6();
+    tetris_sound();
+    while (1) {
+    }
+    return 0;
 }
