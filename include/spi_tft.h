dd7b6e4c2244ce65f1bf9e6a013ecfcbd5cb279b
Arnav Namdev <arnavnamdev@gmail.com>
2025-04-04 21:42:21 -0400

    TFT updates

--- include/spi_tft.h
+++ include/spi_tft.h
@@ -13,6 +13,19 @@ void disable_sdcard(void);
 void init_sdcard_io();
 void sdcard_io_high_speed();
 void init_lcd_spi();
+void draw_I(uint16_t x, uint16_t y, uint16_t c);
+void draw_O(uint16_t x, uint16_t y, uint16_t c);
+void draw_J(uint16_t x, uint16_t y, uint16_t c);
+void draw_L(uint16_t x, uint16_t y, uint16_t c);
+void draw_T(uint16_t x, uint16_t y, uint16_t c);
+void draw_S(uint16_t x, uint16_t y, uint16_t c);
+void draw_Z(uint16_t x, uint16_t y, uint16_t c);
+void draw_cell(uint16_t base_x, uint16_t base_y, uint16_t color);
+
+typedef struct {
+    int x;
+    int y;
+} Point;
 
 
 #endif /* __SPI_TFT_H__ */
