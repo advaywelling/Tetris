830e29a7c752638ad1e25e2fc076a53e7f947efd
yash-ashte <yash.ashte@gmail.com>
2025-04-28 14:38:48 -0400

    eeprom

--- src/i2c.c
+++ src/i2c.c
@@ -1,27 +1,6 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Niraj Menon
-  * @date    Sep 23, 2024
-  * @brief   ECE 362 I2C Lab Student template
-  ******************************************************************************
-*/
-
 #include "stm32f0xx.h"
 #include <stdio.h>
 
-void internal_clock();
-void enable_ports();
-void init_i2c();
-void i2c_start(uint32_t targadr, uint8_t size, uint8_t dir);
-void i2c_stop();
-void i2c_waitidle();
-void i2c_clearnack();
-int i2c_checknack();
-
-//===========================================================================
-// Configure SDA and SCL.
-//===========================================================================
 void enable_ports(void) {
     RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 
     GPIOB->MODER &= ~(0xF << (6 * 2)); 
@@ -29,74 +8,48 @@ void enable_ports(void) {
     GPIOB->AFR[0] |= (1 << (6 * 4)) | (1 << (7 * 4)); 
 }
 
-//===========================================================================
-// Configure I2C1.
-//===========================================================================
 void init_i2c(void) {
     RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;   
-    I2C1->CR1 &= ~I2C_CR1_PE;      
-    I2C1->CR1 |= I2C_CR1_ANFOFF;
-    I2C1->CR1 |= I2C_CR1_ERRIE;
-    I2C1->CR1 |= I2C_CR1_NOSTRETCH; 
-    I2C1->TIMINGR = (0 << 28) | (3 << 20) | (1 << 16) | (3 << 8) | (9 << 0);
-    //PRESC=0, SCLDEL=3, SDADEL=1, SCLH=3, SCLL=9 for 400kHz
+    I2C1->CR1 &= ~I2C_CR1_PE;             
+    I2C1->TIMINGR = (5 << 28) | (4 << 20) | (2 << 16) | (15 << 8) | (21 << 0); //PRESC=5, SCLDEL=4, SDADEL=2, SCLH=15, SCLL=21 for 400kHz
 
-             
-        
+    I2C1->CR1 |= I2C_CR1_ERRIE;         
+    I2C1->CR1 &= ~I2C_CR1_NOSTRETCH;     
     I2C1->CR2 &= ~I2C_CR2_ADD10;     
     I2C1->CR1 |= I2C_CR1_PE;   
 }
 
-//===========================================================================
-// Send a START bit.
-//===========================================================================
 void i2c_start(uint32_t targadr, uint8_t size, uint8_t dir) {
     uint32_t tmpreg = I2C1->CR2;
     tmpreg &= ~(I2C_CR2_SADD | I2C_CR2_NBYTES | I2C_CR2_RD_WRN |
                 I2C_CR2_START | I2C_CR2_STOP);
-    if (dir) {
+    if (dir)
         tmpreg |= I2C_CR2_RD_WRN;
-    }
-    else {
-        tmpreg &= ~I2C_CR2_RD_WRN;
-    }
-    tmpreg |= ((targadr<<1) & I2C_CR2_SADD) | ((size << 16) & I2C_CR2_NBYTES);
-
-    tmpreg |= I2C_CR2_START;
+    tmpreg |= ((targadr << 1) & I2C_CR2_SADD) |
+              ((size << 16) & I2C_CR2_NBYTES) |
+              I2C_CR2_START;
     I2C1->CR2 = tmpreg;
 }
 
-//===========================================================================
-// Send a STOP bit.
-//===========================================================================
 void i2c_stop(void) {
-    //if (I2C1->CR2 & I2C_CR2_STOP) return;
-    if ((I2C1->ISR & I2C_ISR_STOPF)) return;
+    if (I2C1->CR2 & I2C_CR2_STOP) return;
     I2C1->CR2 |= I2C_CR2_STOP;
     while (!(I2C1->ISR & I2C_ISR_STOPF));
     I2C1->ICR |= I2C_ICR_STOPCF;
 }
 
-//===========================================================================
-// Wait until the I2C bus is not busy. (One-liner!)
-//===========================================================================
 void i2c_waitidle(void) {
     while (I2C1->ISR & I2C_ISR_BUSY);
-    return;
 }
 
-//===========================================================================
-// Send each char in data[size] to the I2C bus at targadr.
-//===========================================================================
 int8_t i2c_senddata(uint8_t targadr, uint8_t data[], uint8_t size) {
     i2c_waitidle();
     i2c_start(targadr, size, 0); // Write
 
     for (int i = 0; i < size; i++) {
         int count = 0;
-        while ((I2C1->ISR & I2C_ISR_TXIS) == 0) {
-            count += 1;
-            if (count > 1000000)
+        while (!(I2C1->ISR & I2C_ISR_TXIS)) {
+            if (++count > 1000000)
                 return -1;
             if (i2c_checknack()) {
                 i2c_clearnack();
@@ -109,29 +62,24 @@ int8_t i2c_senddata(uint8_t targadr, uint8_t data[], uint8_t size) {
 
     while (!(I2C1->ISR & I2C_ISR_TC) && !(I2C1->ISR & I2C_ISR_NACKF));
     if (I2C1->ISR & I2C_ISR_NACKF) {
-        //i2c_clearnack();
-        //i2c_stop();
+        i2c_clearnack();
+        i2c_stop();
         return -1;
     }
+
     i2c_stop();
     return 0;
 }
 
-//===========================================================================
-// Receive size chars from the I2C bus at targadr and store in data[size].
-//===========================================================================
-int i2c_recvdata(uint8_t targadr, uint8_t data[], uint8_t size) {
+int i2c_recvdata(uint8_t targadr, void *data, uint8_t size) {
+    uint8_t *data = vdata;
     i2c_waitidle();
     i2c_start(targadr, size, 1); // Read
 
-    // Cast the void pointer to uint8_t pointer
-    //uint8_t *recv_data = (uint8_t *)data;
-
     for (int i = 0; i < size; i++) {
         int count = 0;
-        while ((I2C1->ISR & I2C_ISR_RXNE) == 0) {
-            count += 1;
-            if (count > 1000000)
+        while (!(I2C1->ISR & I2C_ISR_RXNE)) {
+            if (++count > 1000000)
                 return -1;
             if (i2c_checknack()) {
                 i2c_clearnack();
@@ -140,32 +88,18 @@ int i2c_recvdata(uint8_t targadr, uint8_t data[], uint8_t size) {
             }
         }
         data[i] = I2C1->RXDR & I2C_RXDR_RXDATA;
-        if (data[i] == 0)
-            return 0;
     }
-    //data[size] = '\0';
 
     return 0;
 }
 
-//===========================================================================
-// Clear the NACK bit. (One-liner!)
-//===========================================================================
 void i2c_clearnack(void) {
     I2C1->ICR |= I2C_ICR_NACKCF;
 }
 
-//===========================================================================
-// Check the NACK bit. (One-liner!)
-//===========================================================================
 int i2c_checknack(void) {
-    return (I2C1->ISR & I2C_ISR_NACKF);
+    return I2C1->ISR & I2C_ISR_NACKF;
 }
-
-//===========================================================================
-// EEPROM functions
-// We'll give these so you don't have to figure out how to write to the EEPROM.
-// These can differ by device.
 
 #define EEPROM_ADDR 0x57
 
@@ -188,9 +122,6 @@ void eeprom_read(uint16_t loc, char data[], uint8_t len) {
     i2c_recvdata(EEPROM_ADDR, data, len);
 }
 
-//===========================================================================
-// Copy in code from Lab 7 - USART
-//===========================================================================
 
 #include "fifo.h"
 #include "tty.h"
@@ -318,7 +249,7 @@ void write(int argc, char* argv[]) {
         printf("Ensure the address is a hexadecimal number.  No need to include 0x.\n");
         return;
     }
-    uint32_t addr = strtol(argv[1], NULL, 16); 
+    uint32_t addr = atoi(argv[1]); 
     // concatenate all args from argv[2], until empty string is found, to a string
     char data[32] = "";
     int i = 0;
@@ -327,18 +258,12 @@ void write(int argc, char* argv[]) {
         for (char c = argv[j][0]; c != '\0'; c = *(++argv[j])) {
             data[i++] = c;
         }
-        if (strcmp(argv[j+1], "") != 0) {
+        if (strcmp(argv[j+1], "") != 0)
             data[i++] = ' ';
-        }
-        // else {
-        //      data[i + 1] = '\0';
-        // }
-
         j++;
     }
-    
     // ensure addr is a multiple of 32
-    if ((addr % 32) != 0) {
+    if ((addr % 10) != 0) {
         printf("Address 0x%ld is not evenly divisible by 32.  Your address must be a hexadecimal value.\n", addr);
         return;
     }
@@ -357,10 +282,10 @@ void read(int argc, char* argv[]) {
         printf("Ensure the address is a hexadecimal number.  No need to include 0x.\n");
         return;
     }
-    uint32_t addr = strtol(argv[1], NULL, 16); 
+    uint32_t addr = atoi(argv[1]); 
     char data[32];
     // ensure addr is a multiple of 32
-    if ((addr % 32) != 0) {
+    if ((addr % 10) != 0) {
         printf("Address 0x%ld is not evenly divisible by 32.  Your address must be a hexadecimal value.\n", addr);
         return;
     }
@@ -411,38 +336,3 @@ void parse_command(char *c)
         exec(argc, argv);
     }
 }
-
-//===========================================================================
-// main()
-//===========================================================================
-
-// int main() {
-//     internal_clock();
-    
-//     // I2C specific
-//     enable_ports();
-//     init_i2c();
-
-//     // If you don't want to deal with the command shell, you can 
-//     // comment out all code below and call 
-//     // eeprom_read/eeprom_write directly.
-//     init_usart5();
-//     enable_tty_interrupt();
-//     // These turn off buffering.
-//     setbuf(stdin,0); 
-//     setbuf(stdout,0);
-//     setbuf(stderr,0);
-
-//     printf("I2C Command Shell\n");
-//     printf("This is a simple shell that allows you to write to or read from the I2C EEPROM at %d.\n", EEPROM_ADDR);
-//     for(;;) {
-//         printf("\n> ");
-//         char line[100];
-//         fgets(line, 99, stdin);
-//         line[99] = '\0';
-//         int len = strlen(line);
-//         if (line[len-1] == '\n')
-//             line[len-1] = '\0';
-//         parse_command(line);
-//     }
-// }
